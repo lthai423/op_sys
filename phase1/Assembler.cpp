dd7f842bf4f42e649f59207fedb0003b30414861
Lawrence <lthai423@gmail.com>
2016-04-09 12:31:51 -0700

    Tentative prototyped interface for Assembler object

---
+++ phase1/Assembler.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+#include "Assembler.h"
+#include <string>
+
+using namespace std;
+
+class Assembler{
+    public:
+        Assembler(); //main logic
+        string sendFinalObjProg(); //sends catted final prog.o file
+    private:
+        returnFormat1(); //based on flag, use format 1
+        returnFormat2(); //based on flag, use format 2
+        storeSNN(); // store OPCODE and N!, if cin.good() store N2
+        appendToFinal(); //append produced # to final prog.o
+        initializeMemberFields();
+        return_2s_comp_format(); //if num is negative, reformat before adding to instruction
+        
+        map <string, int[]> opcodes;
+        map <int, int> RD;
+        map <int, int> RS;
+        string FinalObjProg;
+        int n1;
+        int n2;
+
+}
+
