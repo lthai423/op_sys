66145d02a7090a9b0f077e23840749eb73926246
Ubuntu <rquon.student@outlook.com>
2016-05-08 21:57:29 -0700

    os.h

---
+++ phase2/os.h
@@ -0,0 +1,24 @@
+#ifndef OS_H
+#define OS_H
+#include<list>
+#include<queue>
+#include<map>
+#include<fstream>
+#include<iostream>
+#include<sstream>
+#include "Assembler.h"
+#include "VirtualMachine.h"
+#include "PCB.h"
+
+using namespace std;
+
+class OS
+{
+private:
+
+public:
+     os();
+     void run();
+};
+
+#endif
